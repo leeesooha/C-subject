280079fbb2d2666ecbdbd88d297f96ca440d04a7
leeesooha <suha630@naver.com>
2023-03-26 15:14:44 +0900

    homecome

--- Borncourse/pipex/test.c
+++ Borncourse/pipex/test.c
@@ -6,29 +6,39 @@
 
 int main(int argc, char **argv, char **envp)
 {
-    int *pipefd;
-    int *infilefd;
-   int   *outfilefd;
-   char  *str[2];
-   int   pid;
+    int     pid;
+    int     pid2;
+    char    *str;
+    int     idx;
+    int     waitstatus;
 
-   str[0] = "cat";
-   str[1] = 0;
-   pipefd = (int *)malloc(4 * 2);
-    pipe(pipefd);
-   infilefd = (int *)malloc(4 * 2);
-
-   pid = fork();
-   if (pid == 0)
-   {
-   infilefd = open("file1", O_RDONLY);
-   outfilefd = open("file3", O_WRONLY);
-   dup2(infilefd, 0);
-   dup2(pipefd[1]);
-   execve("/bin/cat", str, 0);
-   }
-
-   pid = fork();
-   
-   return (0);
-}s
+    str = (char *)malloc(sizeof(char) * 11);
+    idx = 0;
+    while (idx < 10)
+    {
+        str[idx] = idx + 'A';
+        idx++;
+    }
+    str[idx] = 0;
+    pid = fork();
+    waitpid(-1, 0, 0);
+    if (pid)
+    {
+        printf("parent : %p\n", str);
+        printf("parent : %s\n", str);
+    }
+    if (pid == 0)
+    {
+        str[0] = 'Z';
+        printf("child : %p\n", str);
+        printf("child : %s\n", str);
+    }
+    pid2 = fork();
+    if (pid2 == 0)
+    {
+        str[0] = '&';
+        printf("child2 : %p\n", str);
+        printf("child2 : %s\n", str);
+    }
+    return (0);
+}
