d3f68e376cfe64b4a93d8bf41dd082b6bcd9970a
Sooha Lee <soohlee@c1r3s5.42seoul.kr>
2023-04-06 17:54:50 +0900

    0306

--- Borncourse/so_long/map_check2.c
+++ Borncourse/so_long/map_check2.c
@@ -6,25 +6,25 @@
 /*   By: soohlee <soohlee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 13:23:26 by soohlee           #+#    #+#             */
-/*   Updated: 2023/04/05 14:08:04 by soohlee          ###   ########.fr       */
+/*   Updated: 2023/04/05 18:10:07 by soohlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int path_check(t_config *flag)
+int	path_check(t_config *flag)
 {
-    int x;
-    int y;
+	int	x;
+	int	y;
 
-    x = flag->start_xy[0];
-    y = flag->start_xy[1];
-    dfs(x, y, *flag);
-    return (0);
+	x = flag->start_xy[0];
+	y = flag->start_xy[1];
+	dfs(x, y, flag->map, flag);
+	return (0);
 }
 
-int dfs(int x, int y, t_config flag)
+int	dfs(int x, int y, char **map, t_config *flag)
 {
-    
-    return (0);
+	dfs ();
+	return (0);
 }
--- Borncourse/so_long/so_long.h
+++ Borncourse/so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: soohlee <soohlee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/01 20:05:51 by soohlee           #+#    #+#             */
-/*   Updated: 2023/04/05 13:48:49 by soohlee          ###   ########.fr       */
+/*   Updated: 2023/04/05 17:47:43 by soohlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ int	map_check(char *file, char ***map);
 int	read_map(char *file, t_config *flag);
 int	nl_len(char *file, int *buff_len);
 int	config_check(t_config *flag, int map_row_len, int buff_len);
-int flagging(char c, t_config *flag, int idc, int jdx);
+int	flagging(char c, t_config *flag, int idc, int jdx);
 
 /*
 error & free
